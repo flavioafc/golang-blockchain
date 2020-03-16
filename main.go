3404cf3308114c2ddbad41ed58d76d2ce56068e7
Flavio Aurelio Frazao Costa <flavio_costa_dev@hotmail.com>
2020-03-16 10:38:41 -0300

    folder blockchain

--- main.go
+++ main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"fmt"
 
-	"rsc.io/quote"
+	"github.com/flavioafc/golang-blockchain/blockchain"
 )
 
 func main() {
-	fmt.Println(quote.Hello())
-	fmt.Printf("Hello world com printf em casa")
+
+	chain := blockchain.InitBlockChain()
+
+	chain.AddBlock("First Block after Genesis")
+	chain.AddBlock("Second Block after Genesis")
+	chain.AddBlock("Third Block after Genesis")
+
+	for _, block := range chain.Blocks {
+		fmt.Printf("Previews block: %x\n", block.PrevHash)
+		fmt.Printf("Data in block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Printf("-----------------------------\n")
+	}
+
 }
